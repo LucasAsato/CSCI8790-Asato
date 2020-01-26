0c51a2fbfd3a72c58046e4977c9e6a445ee70f44
LucasAsato <lasato@unomaha.edu>
2020-01-26 17:12:54 -0500

    add target classes and finished analysis program

--- javassist-project-Asato-0123/src/ex04/toclass.java
+++ javassist-project-Asato-0123/src/ex04/toclass.java
@@ -1,6 +1,7 @@
 package ex04;
 
 import java.lang.reflect.InvocationTargetException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,6 +9,7 @@ import java.util.Scanner;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.NotFoundException;
 
 public class toclass
@@ -20,29 +22,43 @@ public class toclass
 		while (true) 
 		{
 			System.out.println("=============================================");
-			System.out.print("Enter one class (\"q\" to terminate)\n");
+			System.out.print("Enter three class (e.g., CommonComponentB,id,year or CommonServiceA,id,year\")\n"
+					+ "(\"q\" to terminate)\n");
 		    String[] inputs = getInputs();
-		    if (inputs.length != 1)
+		    if (inputs.length != 3)
 		    {
 		    	System.out.println("[WRN] Invalid Input");
 		    }
 		    else
 		    {
-		    	try
-		    	{
-		    		ClassPool cp = ClassPool.getDefault();
-			        CtClass cc = cp.get(PKG_NAME + inputs[0]);
-		    	}
-		    	catch (NotFoundException | CannotCompileException | //
-		    	            InstantiationException | IllegalAccessException | //
-		    	            NoSuchMethodException | SecurityException | //
-		    	            IllegalArgumentException | InvocationTargetException e) 
-		    	{
-		    		System.out.println(e.toString());
-		    	}
+		    	process(inputs[0], inputs[1], inputs[2]);
 		    }
 		}
 	}
+	static void process(String clazz, String field1, String field2)
+	{
+	      try {
+	         ClassPool cp = ClassPool.getDefault();
+	         CtClass cc = cp.get(PKG_NAME + clazz);
+
+	         CtConstructor declaredConstructor = cc.getDeclaredConstructor(new CtClass[0]);
+	         String block1 = "{ " //
+	               + "System.out.println(\"[TR] " + field1 + ": \" + " + field1 + "); }";
+	         System.out.println("[DBG] BLOCK1: " + block1);
+	         declaredConstructor.insertAfter(block1);
+
+	         String block2 = "{ " //
+	               + "System.out.println(\"[TR] " + field2 + ": \" + " + field2 + "); }";
+	         System.out.println("[DBG] BLOCK2: " + block2);
+	         declaredConstructor.insertAfter(block2);
+	         Class<?> c = cc.toClass();
+	         c.newInstance();
+	         
+	      } catch (NotFoundException | CannotCompileException | //
+	            InstantiationException | IllegalAccessException e) {
+	         System.out.println(e.toString());
+	      }
+	   }
 	public static String[] getInputs() 
 	{
 		String input = sc.nextLine();
